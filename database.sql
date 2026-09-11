ce6274e353a781f7405871919736324937e2d2ca
Leo Thai <lthai1@umbc.edu>
2026-09-11 00:38:34 -0400

    creating tables

--- database.sql
+++ database.sql
@@ -7,8 +7,33 @@ CREATE DATABASE IF NOT EXISTS retriever_dashboard;
 
 USE retriever_dashboard;
 
+-- ============================================================
+-- table for departments
+-- ============================================================
+
 
 CREATE TABLE departments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- ============================================================
+-- table for student information
+-- ============================================================
+
+
+CREATE TABLE studentInfo(
+    id INT PRIMARY KEY UNIQUE,
+    name VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+-- ============================================================
+-- table for 
+-- ============================================================
+
+
+CREATE TABLE (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL UNIQUE
 );
